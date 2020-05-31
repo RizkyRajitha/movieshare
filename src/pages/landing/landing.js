4d2faa30609b593af965d71620263d63f871243a
rizkyrajitha <rajithagunathilake@gmail.com>
2020-05-31 13:35:10 +0530

    added loader

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -14,9 +14,9 @@ import "react-toastify/dist/ReactToastify.css";
 
 const api = "https://movieshare.netlify.app/";
 
-const API = "http://localhost:9000";
+// const API = "http://localhost:9000";
 
-// const API = "https://movieshare.netlify.app/.netlify/functions";
+const API = "https://movieshare.netlify.app/.netlify/functions";
 
 // const API = 'http://localhost:9000/getmovielist'
 
@@ -77,6 +77,7 @@ class Movie extends React.Component {
       imdbcode: [],
       urlmoviedata: [],
       sharername: "",
+      loading: true,
     };
     this.getOptions = _.debounce(this.getOptions.bind(this), 500);
   }
@@ -93,7 +94,7 @@ class Movie extends React.Component {
     Axios.get(`${API}/getmovielist`, config)
       .then((result) => {
         console.log(result.data);
-
+        this.setState({ loading: false });
         result.data.data.forEach((element) => {
           // console.log(element.fields);
 
@@ -118,6 +119,7 @@ class Movie extends React.Component {
         });
       })
       .catch((err) => {
+        this.setState({ loading: false });
         console.log(err);
       });
   }
@@ -291,7 +293,7 @@ class Movie extends React.Component {
         console.log(result.data);
 
         if (result.data.data.deleted) {
-          console.log("de;eted ");
+          // console.log("de;eted ");
 
           var mpped = this.state.movies.map((ele) => ele.id);
 
@@ -354,6 +356,48 @@ class Movie extends React.Component {
           </a>
         </nav>
         <div>
+          <div hidden={!this.state.loading}>
+            {/* <svg
+              xmlns="http://www.w3.org/2000/svg"
+              // xmlns:xlink="http://www.w3.org/1999/xlink"
+              style={{
+                margin: "auto",
+                background: "none repeat scroll 0% 0%",
+                display: "block",
+              }}
+              width="200px"
+              height="200px"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="xMidYMid"
+            >
+              <path
+                fill="none"
+                stroke="#007bff"
+                stroke-width="8"
+                stroke-dasharray="223.23236755371093 33.35656066894532"
+                d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z"
+                stroke-linecap="round"
+                style={{ transform: "scale(0.8);transform-origin:50px 50px" }}
+              >
+                <animate
+                  attributeName="stroke-dashoffset"
+                  repeatCount="indefinite"
+                  dur="1s"
+                  keyTimes="0;1"
+                  values="0;256.58892822265625"
+                ></animate>
+              </path>
+            </svg> */}
+
+            <div className="d-flex justify-content-center text-primary">
+              <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            </div>
+
+            <p className="loadingtext">Crunching latest data for you</p>
+          </div>
+
           <ToastContainer
             position="bottom-left"
             autoClose={4000}
